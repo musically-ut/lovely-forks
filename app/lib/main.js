a5429590312ce43effd710e22d8892c33b571d85
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-07-04 11:29:01 +0200

    Load the style file in Firefox version as well.

--- app/lib/main.js
+++ app/lib/main.js
@@ -10,9 +10,19 @@ require('sdk/preferences/service').set('extensions.sdk.console.logLevel', 'debug
 var data = require('sdk/self').data;
 var { PageMod } = require('sdk/page-mod');
 
+// Work only on Github URLs
+var matchPatterns = [ '*.github.com' ];
+
 // Create a content script
-var pageMod = PageMod({
-    include: ['*.github.com'], // Work only on Github URLs
-    contentScriptFile: [data.url('contentscript.js')],
-    contentScriptWhen: 'end'
+var scriptMod = PageMod({
+    include           : matchPatterns,
+    contentScriptFile : [data.url('contentscript.js')],
+    contentScriptWhen : 'end'
+});
+
+// Load the CSS file at document start
+var cssMod = PageMod({
+    include           : matchPatterns,
+    contentStyleFile  : [data.url('style.css')],
+    contentScriptWhen : 'start'
 });
