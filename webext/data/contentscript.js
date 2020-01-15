f3ba9e356523b9bb8dcf345a65c06c4e126fb22b
Utkarsh Upadhyay <musically.ut@gmail.com>
2020-01-15 12:16:23 +0100

    Handle changed GitHub layout.

--- webext/data/contentscript.js
+++ webext/data/contentscript.js
@@ -87,7 +87,7 @@ function getForksElement () {
 
   // If the layout of the page changes, we'll have to change this location.
   // We should make sure that we do not accidentally cause errors here.
-  const repoName = document.querySelector('.repohead-details-container .public')
+  const repoName = document.querySelector('.repohead > div > div')
   if (repoName) {
     try {
       text = document.createElement('span')
