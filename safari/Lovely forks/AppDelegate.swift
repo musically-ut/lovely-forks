a72ac0699ff431c655c97455376c599fb60ab4f6
ShotSkydiver <bigglesworth330@gmail.com>
2020-07-23 12:37:18 -0600

    Add Safari 12+ extension

---
+++ safari/Lovely forks/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Lovely forks
+//
+//  Created by Conner Owen on 7/7/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
