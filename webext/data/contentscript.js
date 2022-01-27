e9e0cb77c4cdcfe10e2ba4dfcb3fe337927b6caf
Jorgen1040 <26395020+Jorgen1040@users.noreply.github.com>
2022-01-27 18:56:50 +0100

    Fix `also forked` disappearing

--- webext/data/contentscript.js
+++ webext/data/contentscript.js
@@ -170,7 +170,6 @@ function safeUpdateDOM (action, actionName) {
 }
 
 function showDetails (fullName, url, numStars, remoteIsNewer, indented) {
-  if (text.parentNode.classList.contains('has-lovely-forks')) return
   const forkA = document.createElement('a')
   forkA.href = url
   forkA.append(fullName)
